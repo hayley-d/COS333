4d471be6f72374c24b82ea814834dc0c6a28de10
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2025-03-05 12:34:57 +0200

    Add files via upload

--- practical_2/cylinderArea.rkt
+++ practical_2/cylinderArea.rkt
@@ -14,8 +14,20 @@
         (+ x (addPositiveEvenValues (cdr lst)))
         (addPositiveEvenValues (cdr lst))))))
 
+(define (getEveryThirdElement lst)
+  (define (helper lst idx)
+    (cond
+      ((null? lst) '())
+      ((= (remainder idx 3) 2)
+       (cons (car lst) (helper (cdr lst) (+ idx 1))))
+      (else (helper (cdr lst) (+ idx 1)))))
+    (helper lst 0))
+
 (cylinderArea 3.2 2.1)
 (addPositiveEvenValues '())
 (addPositiveEvenValues '(-8 0 7))
 (addPositiveEvenValues '(3 4 0 -8 6))
+(getEveryThirdElement '())
+(getEveryThirdElement '(a b))
+(getEveryThirdElement '(a b c d e f g))
 
