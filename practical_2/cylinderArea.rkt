8f003294991f1d4034db967c4c343b7d1894349d
Hayley Dodkins <91600502+hayley-d@users.noreply.github.com>
2025-03-05 12:23:07 +0200

    add add function

--- practical_2/cylinderArea.rkt
+++ practical_2/cylinderArea.rkt
@@ -1,10 +1,21 @@
 #lang racket
 
-(define (area rad height)
+(define (cylinderArea rad height)
   (if (or (<= rad 0) (<= height 0))
     0
     (let ((pi (/ 22 7)))
       (+ (* 2 pi rad height) (* 2 pi rad rad)))))
 
-(area 3.2 2.1)
+(define (addPositiveEvenValues lst)
+  (if (null? lst)
+    0
+    (let ((x (car lst)))
+      (if (and (> x 0) (even? x))
+        (+ x (addPositiveEvenValues (cdr lst)))
+        (addPositiveEvenValues (cdr lst))))))
+
+(cylinderArea 3.2 2.1)
+(addPositiveEvenValues '())
+(addPositiveEvenValues '(-8 0 7))
+(addPositiveEvenValues '(3 4 0 -8 6))
 
