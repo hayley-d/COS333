d7f00a7558f9986bffd3559ebc8005142a5af7ab
hayley-d <hayleydod@proton.me>
2025-03-22 18:30:40 +0200

    surface area code kata

--- functional_practice/surface_area.rkt
+++ functional_practice/surface_area.rkt
@@ -3,10 +3,18 @@
 (define (surface_area width height depth)
     (cons (* width height) (* (* width height) depth)))
 
+(define (func width height depth)
+  (list (* 2 (+ (* width height) (* width depth) (* height depth)))
+        (* (* width height) depth)))
+
 (if (pair? (surface_area 2 2 2))
   "True"
   "False")
 
 (if (equal? (surface_area 2 2 2) (cons 4 8))
+  "True"
+  "False")
+
+(if (equal? (func 4 2 6) '(88 48))
   "True"
   "False")
