743e9f63386f31ef0c9bd4c267893b69fadaf2da
hayley-d <hayleydod@proton.me>
2025-03-29 18:13:27 +0200

    add prolog stuff

---
+++ practical_3/u21528790.pl
@@ -0,0 +1,49 @@
+% TASK 1
+% Facts
+person(peter).
+person(mary).
+person(tom).
+person(lilly).
+person(joseph).
+person(rob).
+
+married(peter, mary).
+married(lilly, joseph).
+
+invited(mary, tom).
+
+% RULES
+
+liveTogether(X, Y) :- married(X, Y).
+liveTogether(X, Y) :- married(Y, X).
+
+visiting(X, Y) :- invited(Y, X).
+visiting(X, Y) :-
+    liveTogether(Y, Z),
+    invited(Z, X).
+
+livesAlone(X) :-
+    person(X),
+    \+ liveTogether(X, _),
+    \+ visiting(X, _).
+
+% TASK 2
+getZeroValues([], []) :- !.
+
+getZeroValues([0 | T], [0 | ZT]) :-
+    getZeroValues(T, ZT), !.
+
+getZeroValues([H | T], ZT) :-
+    H =\= 0,
+    getZeroValues(T, ZT), !.
+
+% TASK 3 
+monotonicallyDecreasing([]) :- !.
+
+monotonicallyDecreasing([_]) :- !.
+
+monotonicallyDecreasing([H1, H2 | T]) :-
+    H1 >= H2,
+    monotonicallyDecreasing([H2 | T]), !.
+
+
