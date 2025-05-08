8f3db527bf47203878be6a355196bf192de27ed8
hayley-d <hayleydod@proton.me>
2025-05-08 09:35:58 +0200

    Add rules example

---
+++ sem_test_2_prac/witcher_rules.pl
@@ -0,0 +1,13 @@
+% Define Simple Rules
+witcher(geralt).
+witcher(lambert).
+
+monster(leshen).
+monster(griffin).
+
+allies(geralt,yen).
+allies(geralt,triss).
+
+enemies(geralt,eredin).
+enemies(yen,triss).
+enemies(yen, eredin).
