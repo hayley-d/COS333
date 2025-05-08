cceea34191e96607530e3de5348f6e83232b6d62
hayley-d <hayleydod@proton.me>
2025-05-08 16:13:37 +0200

    figured out

--- sem_test_2_prac/practice.pl
+++ sem_test_2_prac/practice.pl
@@ -35,3 +35,34 @@ witcher_without_monster(X) :-
     witcher(X),
     \+ contract(X,_,_).
 
+evolves_into(igglybuff, jigglytuff).
+evolves_into(jigglypuff, wigglytuff).
+evolves_into(happiny, chansey).
+evolves_into(chansey, blissey).
+evolves_into(cleffa, clefairy).
+evolves_into(clefairy, clefable).
+evolves_into(gastly, haunter).
+evolves_into(haunter, gengar).
+
+trainer(hayley, gengar).
+trainer(ash, gastly).
+trainer(mallow, happiny).
+trainer(hayley, clefable).
+
+trainers_evolve_pokemon(Trainer) :-
+    trainer(Trainer,Poke),
+    trainer(_, Y),
+    same_evo_tree(Y,Poke).
+
+    
+
+
+same_evo_tree(X,Y) :- evolves_into(X,Y);evolves_into(Y,X).
+same_evo_tree(X,Y) :- 
+    evolves_into(Y,Z),
+    evolves_into(Z,X);
+    evolves_into(X,Z),
+    evolves_into(Z,Y).
+
+
+
