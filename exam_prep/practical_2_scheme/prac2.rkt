de24f86e3cb6bd1a5742f1d2ec151bcccdf02ce3
hayley-d <hayleydod@proton.me>
2025-06-16 14:11:03 +0200

    finish prac 2

--- exam_prep/practical_2_scheme/prac2.rkt
+++ exam_prep/practical_2_scheme/prac2.rkt
@@ -11,7 +11,13 @@
       [else (helper curr (cdr lst))]))
   (helper 0 lst))
 
-  
+(define (addValues lst)
+  (define (helper curr lst)
+    (cond
+      [(empty? lst) '()]
+      [(and (> (car lst) 0) (isEven (car lst))) (cons (+ curr (car lst)) (helper (+ curr (car lst)) (cdr lst)))]
+      [else (helper curr (cdr lst))]))
+  (helper 0 lst))
 (define (isEven n)
   (cond
     [(= 0 n) #t]
@@ -23,8 +29,21 @@
     [(< n 0) (* -1 n)]
     [else n]))
 
-(cylinderArea 3.2 2.2)
-(addPositiveEvenValues '())
-(addPositiveEvenValues '(1))
-(addPositiveEvenValues '(1 2 3 4))
+(define (getEveryThirdElement lst)
+  (define (helper curr lst)
+    (cond
+      [(empty? lst) '()]
+      [(= (modulo curr 3) 0) (cons (car lst) (helper (+ curr 1) (cdr lst)))]
+      [else (helper (+ curr 1) (cdr lst))]))
+  (helper 1 lst))
+
+(define (modulo a b)
+  (cond
+    [(< a b) a]
+    [else (modulo (- a b) b)]))
+
+
+(getEveryThirdElement '())
+(getEveryThirdElement '(1 2 3))
+(getEveryThirdElement '(1 2 3 4 5 6 7 8 9))
  
